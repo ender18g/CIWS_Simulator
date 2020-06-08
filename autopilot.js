b5cd074ee75c6f4579c38c4f5e8e31ee270b5249
Allan Elsberry <allan.elsberry@gmail.com>
2020-06-08 16:30:49 -0400

    created plot hide function

--- autopilot.js
+++ autopilot.js
@@ -18,10 +18,13 @@ const disturbanceSlider = document.querySelector('#disturbanceSlider');
 const controllerEqn = document.querySelector('#controllerEqn');
 const plotBtn = document.querySelector("#plotBtn");
 const plotPanel = document.querySelector('#plotPanel');
+const directionsPanel=document.querySelector('#directionsPanel');
+const hideDirBtn=document.querySelector('#hideDirBtn');
+
 
 const width = skyPanel.clientWidth;
 const height = skyPanel.clientHeight;
-const maxAlt= 5000;
+const maxAlt = 5000;
 
 
 var render = Render.create({
@@ -37,17 +40,17 @@ var render = Render.create({
 
 
 
-var topWall = Bodies.rectangle(width/2, 0, width, 20, { isStatic: true,});
-var leftWall = Bodies.rectangle(0, height/2, 20, height, { isStatic: true });
-var rightWall = Bodies.rectangle(width, height/2, 20, height, { isStatic: true });
-var bottomWall = Bodies.rectangle(width/2, height, width, 20, { isStatic: true });
+var topWall = Bodies.rectangle(width / 2, 0, width, 20, { isStatic: true, });
+var leftWall = Bodies.rectangle(0, height / 2, 20, height, { isStatic: true });
+var rightWall = Bodies.rectangle(width, height / 2, 20, height, { isStatic: true });
+var bottomWall = Bodies.rectangle(width / 2, height, width, 20, { isStatic: true });
 
-topWall.render.opacity=0;
-leftWall.render.opacity=0;
-rightWall.render.opacity=0;
-bottomWall.render.opacity=.5;
+topWall.render.opacity = 0;
+leftWall.render.opacity = 0;
+rightWall.render.opacity = 0;
+bottomWall.render.opacity = .5;
 
-var heloBody = Bodies.circle(width/2, 280, 20, {
+var heloBody = Bodies.circle(width / 2, 280, 20, {
   render: {
     sprite: {
       texture: "sh60.png",
@@ -63,99 +66,129 @@ Engine.run(engine);
 
 Render.run(render);
 
-let autoPilotOn=false;
-let altArray = Array(1500).fill(0);
+plotLength = 5;
+let autoPilotOn = false;
+let altArray = Array(100 * plotLength).fill(0);
 let errArray = Array(1000).fill(0);
+heloBody.frictionAir = .03;
+let showPlot = false;
+//plot length in seconds
+
 
 let helo = {
-  getAltitude(){
-    let altitude = (height-heloBody.position.y)/height * maxAlt;
-    altitudeText.innerText=altitude.toFixed(0);
+  getAltitude() {
+    let altitude = (height - heloBody.position.y) / height * maxAlt;
+    altitudeText.innerText = altitude.toFixed(0);
     return altitude;
   },
-  getThrottle(){
+  getThrottle() {
     return parseFloat(throttleSlider.value);
   },
-  applyForce(){
-    let forceFactor = (this.getThrottle()-Math.pow(this.getAltitude(),2)*.000002)
-    let randomFactor = (Math.random()*(2)-1)*disturbanceSlider.value;
+  applyForce() {
+    let forceFactor = (this.getThrottle() - Math.pow(this.getAltitude(), 2) * .000002)
+    let randomFactor = (Math.random() * (2) - 1) * disturbanceSlider.value;
     Body.applyForce(
       heloBody,
       { x: heloBody.position.x, y: heloBody.position.y },
-      { x: 0, y: -0.000015*forceFactor+.00001*randomFactor}
+      { x: 0, y: -0.000015 * forceFactor + .00001 * randomFactor }
     );
   },
-  applyController(){
-    const y=parseInt(desiredAltSlider.value);
-    desiredAltText.innerText=y;
-    const currentAlt=this.getAltitude();
+  applyController() {
+    const y = parseInt(desiredAltSlider.value);
+    desiredAltText.innerText = y;
+    const currentAlt = this.getAltitude();
     altArray.pop();
     altArray.unshift(currentAlt);
     errArray.pop();
-    errArray.unshift(y-currentAlt);
-    const e = errArray.reduce((acc,val)=>{
-      return acc+=val/errArray.length;
+    errArray.unshift(y - currentAlt);
+    const e = errArray.reduce((acc, val) => {
+      return acc += val / errArray.length;
     })
-    s=altArray;
-    throttleSlider.value=parseInt(eval(controllerEqn.value));
+    s = altArray;
+    throttleSlider.value = parseInt(eval(controllerEqn.value));
 
   }
 }
 
 
 //continuously applies throttle force
-setInterval(()=>{
-  if (autoPilotOn){
+setInterval(() => {
+  if (autoPilotOn) {
     helo.applyController();
   }
   helo.applyForce();
-},10)
+}, 10)
 
 
-autoPilotBtn.addEventListener('click',()=>{
-  if(autoPilotOn){
+autoPilotBtn.addEventListener('click', () => {
+  if (autoPilotOn) {
     //turn autopilot off
     autoPilotBtn.classList.add('btn-danger');
     autoPilotBtn.classList.remove('btn-success');
-    autoPilotBtn.innerText='OFF';
-    autoPilotOn=false;
+    autoPilotBtn.innerText = 'OFF';
+    autoPilotOn = false;
   }
-  else{
+  else {
     //turn autopilot on
     autoPilotBtn.classList.remove('btn-danger');
     autoPilotBtn.classList.add('btn-success')
-    autoPilotBtn.innerText='ON';
-    autoPilotOn=true;
+    autoPilotBtn.innerText = 'ON';
+    autoPilotOn = true;
   }
 
 });
 
-plotBtn.addEventListener('click',()=>{
-  if (autoPilotOn){
-    autoPilotBtn.click();
-  }
-  plotPanel.innerHTML='';
-  throttleSlider.value=0;
-  setTimeout(()=>{
-    autoPilotBtn.click();
-    setTimeout(()=>{
-      makePlot(parseInt(desiredAltSlider.value),altArray.reverse(),plotPanel)
+plotBtn.addEventListener('click', () => {
+  if (!showPlot) {
+    showPlot=true;
+    plotBtn.classList.add('bg-danger');
+    plotBtn.classList.remove('bg-warning');
+    if (autoPilotOn) {
+      autoPilotBtn.click();
     }
-    ,15000);
-  },1000);
-  
+    plotBtn.innerText = plotLength + 1;
+    let countDownID = setInterval(() => {
+      plotBtn.innerText = parseInt(plotBtn.innerText) - 1;
+      if (plotBtn.innerText === '0') {
+        clearInterval(countDownID);
+        plotBtn.innerText = 'Remove Plot';
+      }
+    }, 1000);
+    plotPanel.innerHTML = '';
+    throttleSlider.value = 0;
+    setTimeout(() => {
+      autoPilotBtn.click();
+      setTimeout(() => {
+        plotPanel.classList.remove('d-none')
+        skyPanel.classList.remove('d-none')
+        makePlot(parseInt(desiredAltSlider.value), altArray.reverse(), plotPanel)
+      }
+        , plotLength * 1000);
+    }, 1000);
+  }
+  else{
+    showPlot=false;
+    plotPanel.classList.add('d-none');
+    plotBtn.classList.remove('bg-danger');
+    plotBtn.classList.add('bg-warning');
+
+    plotBtn.innerText='Create Plot';
+  }
 });
 
 
+hideDirBtn.addEventListener('click',()=>{
+directionsPanel.classList.add('d-none');
+});
 
 
 // MAKE THE PLOT *******************
 
-const makePlot = (requestedInput,myArray, myDiv) => {
+const makePlot = (requestedInput, myArray, myDiv) => {
   const timeArray = [];
   const inputArray = [];
   for (let i = 0; i < myArray.length; i++) {
-    timeArray.push(i);
+    timeArray.push(i * 10);
     inputArray.push(requestedInput);
   }
   var trace1 = {
@@ -176,7 +209,7 @@ const makePlot = (requestedInput,myArray, myDiv) => {
     }
   };
 
-  var data = [trace1,trace2];
+  var data = [trace1, trace2];
   var layout = {
     title: 'Altitude Response',
     xaxis: {
@@ -186,5 +219,6 @@ const makePlot = (requestedInput,myArray, myDiv) => {
       title: 'Altitude (feet)'
     }
   };
-  Plotly.newPlot(myDiv, data,layout);
+  Plotly.newPlot(myDiv, data, layout);
 }
+
